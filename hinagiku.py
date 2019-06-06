c4eaa631776cbd221eb4a67a9caf9d85af6c7944
hozukiMk3 <meta.maetel@gmail.com>
2019-06-06 19:12:17 +0900

    feature UD

--- hinagiku.py
+++ hinagiku.py
@@ -2,6 +2,8 @@ import discord
 import random
 
 doseisasnn_user_name = 'doseisann'
+debug_room_name = 'yuppibot-debug'
+
 
 class HinagikuClient(discord.Client):
     async def on_ready(self):
@@ -9,17 +11,23 @@ class HinagikuClient(discord.Client):
         for n in self.get_all_members():
             if n.name == doseisasnn_user_name:
                 self.doseisann = n
+        for channel in self.get_all_channels():
+            if channel.name == debug_room_name:
+                self.target_channel = channel
 
     async def on_message(self, message):
+        # 発言者がdoseisannの時一定確率で無視する
         if message.author == self.doseisann:
             if random(1):
                 return
-        if '下' in message.content or 'した' in message.content:
-            await message.channel.send('UD!')
+        # UD機能
+        if '下' in message.content or 'した' in message.content or 'sita' in message.content.lower():
+            await self.target_channel.send('UD!')
 
 
-def random(boarder,max = 3):
-    return random.randint(0,max) <= boarder
+def random(boarder, max=3):
+    return random.randint(0, max) <= boarder
+
 
 def main():
     token = ''
