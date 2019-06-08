6e816d57c3b30c80d3860bfb24c88c17c551a4e1
hozuki <meta.maetel@gmail.com>
2019-06-08 21:20:18 +0900

    feature #6 酒の呼びかけで金晩モードに入る
    Debug用に30秒だけ

--- hinagiku.py
+++ hinagiku.py
@@ -2,6 +2,8 @@ import discord
 import random
 import asyncio
 import time
+import date_ext
+import datetime
 
 doseisasnn_user_name = 'doseisann'
 debug_room_name = 'yuppibot-debug'
@@ -20,6 +22,8 @@ class HinagikuClient(discord.Client):
         self.said_ecc_opening = False
         self.said_ecc_closing = False
         self.kagisime_ozisann = False
+        self.is_drinking = False
+        self.drinking_time_end = None
         self.gerotter_counter = 0
         self.GERO_LIMIT = 3
         asyncio.ensure_future(self.schedule_pending())
@@ -32,20 +36,26 @@ class HinagikuClient(discord.Client):
         # UD機能
         if '下' in message.content or 'した' in message.content or 'sita' in message.content.lower():
             await self.target_channel.send('UD!')
-        
+
+        # Yukitterモード
         if message.content.lower() == 'yukitterの真似して':
-            if True:
+            if self.is_gerotter_mode():
                 self.gerotter_counter += 1
                 if self.gerotter_counter > self.GERO_LIMIT:
                     self.gerotter_counter = 0
-                    await self.target_channel.send('ｺﾞﾒﾝ',file=discord.File('./Resource/gerotter.jpg'))
+                    await self.target_channel.send('ｺﾞﾒﾝ', file=discord.File('./Resource/gerotter.jpg'))
                 else:
                     urtla = ''
                     for n in range(self.gerotter_counter):
                         urtla += 'ﾍｱｯ'
                     await self.target_channel.send(urtla)
             else:
-                await self.target_channel.send('' ,file=discord.File('./Resource/gone.jpg'))
+                await self.target_channel.send('', file=discord.File('./Resource/gone.jpg'))
+
+        if self.is_drinking_mode_message(message.content, message.mentions):
+            self.is_drinking = True
+            self.drinking_time_end = datetime.datetime.now() + datetime.timedelta(seconds=30)
+            await self.target_channel.send("debug:泥酔タイム開始")
 
     async def on_ecc_state_changed(self, message):
         await self.target_channel.send(message)
@@ -68,7 +78,18 @@ class HinagikuClient(discord.Client):
                 self.said_ecc_opening = False
                 self.said_ecc_closing = False
                 self.kagisime_ozisann = False
+
+            if self.is_drinking and datetime.datetime.now() > self.drinking_time_end:
+                self.drinking_time_end = None
+                self.is_drinking = False
+                await self.target_channel.send("debug:泥酔タイム終了")
             await asyncio.sleep(10)
+
+    def is_gerotter_mode(self):
+        return date_ext.is_friday() or self.is_drinking
+
+    def is_drinking_mode_message(self, message, mention):
+        return '酒' in message or '飲' in message or self.user in mention
 
 
 def random(boarder, max=3):
