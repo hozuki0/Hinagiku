fb2fd6e1db9e70d15705c41fc84f4d6f70c3d57c
hozukiMk3 <meta.maetel@gmail.com>
2019-06-10 14:13:29 +0900

    feature #4 ダイスの数とダイスの面の数のパースを終了
    d,D両方に対応しているがそのほかのSyntaxSugarはまだ

---
+++ re_test.py
@@ -0,0 +1,38 @@
+import re
+import sys
+
+
+def main():
+    test_message = [
+        '1d6+10',
+        '1d6+10 < 5',
+        '1d6+10 < 5 [RoR2]',
+
+        '1D6+10',
+        '1D6+10 < 5',
+        '1D6+10 < 5 [RoR2]'
+    ]
+    for n in test_message:
+        print(is_dice_roll(n))
+        print(parse_dice(n))
+
+def is_dice_roll(message):
+    return ('d' in message or 'D' in message) and '+' in message
+
+def parse_dice(message):
+    lower_message = message.lower()
+    first_d_pos = lower_message.find('d')
+    temp_str_array = []
+    for n in range(first_d_pos):
+        temp_str_array += [lower_message[n]]
+    dice_count = int(''.join(temp_str_array))
+    lower_message = lower_message[first_d_pos + 1:len(lower_message) - first_d_pos - 1]
+    first_plus_pos = lower_message.find('+')
+
+    temp_str_array = []
+    for n in range(first_plus_pos):
+        temp_str_array += [lower_message[n]]
+    dice_side = int(''.join(temp_str_array))
+    return (dice_count,dice_side)
+if __name__ == '__main__':
+    main()
