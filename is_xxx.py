e46bc6ef6c051435d9114e293f50a5f2a7494e18
hozuki <meta.maetel@gmail.com>
2019-06-23 01:12:57 +0900

    feature #13 IsXXX()の構文糖衣に向けて

---
+++ is_xxx.py
@@ -0,0 +1,19 @@
+import re
+
+
+def main():
+    test_case = [
+        ['IsRoR2()', 'RoR2'],
+    ]
+
+
+def is_isXXX_format(message):
+    pass
+
+
+def parse_function_name(message):
+    pass
+
+
+if __name__ == '__main__':
+    main()
