9e452d3554b9adf6528c89141e814f264d00dbec
hozuki <meta.maetel@gmail.com>
2020-02-27 07:34:51 +0900

    refactor #17 消すか迷ったけどしゃーなし残す判断

--- go/utility/utility.go
+++ go/utility/utility.go
@@ -2,7 +2,7 @@ package utility
 
 import "time"
 
-// DelayFunction ...
+// DelayFunction 指定時間後に指定関数を実行する
 func DelayFunction(callback func(), delaySec time.Duration) {
 	time.AfterFunc(delaySec*time.Second, callback)
 }
